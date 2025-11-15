80924377b3e9afa94936fcddaa1a0a2e2068d399
AR Janabi Personal Ubuntu <aaljanabi1992@gmail.com>
2025-11-15 23:30:00 +0300

    add a light and dark moon across the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider } from './context/ThemeContext';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
@@ -11,18 +12,20 @@ import './App.css';
 function App() {
   return (
     <HelmetProvider>
-      <Router>
-        <div className="app">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blog/:slug" element={<BlogPost />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/playground" element={<Playground />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
+      <ThemeProvider>
+        <Router>
+          <div className="app">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/blog/:slug" element={<BlogPost />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/playground" element={<Playground />} />
+            </Routes>
+            <Footer />
+          </div>
+        </Router>
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
